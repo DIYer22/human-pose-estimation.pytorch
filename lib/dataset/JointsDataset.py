9c8a7605616f9adf6a6000b04472987f8365fefa
Lei <ylxx@live.com>
2019-03-09 05:39:24 +0800

    chang jointsDataset

--- lib/dataset/JointsDataset.py
+++ lib/dataset/JointsDataset.py
@@ -184,17 +184,22 @@ class JointsDataset(Dataset):
         target_weight = np.ones((self.num_joints, 1), dtype=np.float32)
         target_weight[:, 0] = joints_vis[:, 0]
 
+        joints = copy.deepcopy(joints)
+        joints_vis = copy.deepcopy(joints_vis)
         assert self.target_type == 'gaussian', \
             'Only support gaussian map now!'
 
         if self.target_type == 'gaussian':
+            
+            # TODO
             target = np.zeros((self.num_joints,
                                self.heatmap_size[1],
                                self.heatmap_size[0]),
                               dtype=np.float32)
-
+#            target = target_weight
+            
             tmp_size = self.sigma * 3
-
+            tmp_size = 0
             for joint_id in range(self.num_joints):
                 feat_stride = self.image_size / self.heatmap_size
                 mu_x = int(joints[joint_id][0] / feat_stride[0] + 0.5)
@@ -207,25 +212,25 @@ class JointsDataset(Dataset):
                     # If not, just return the image as is
                     target_weight[joint_id] = 0
                     continue
-
+                
                 # # Generate gaussian
-                size = 2 * tmp_size + 1
-                x = np.arange(0, size, 1, np.float32)
-                y = x[:, np.newaxis]
-                x0 = y0 = size // 2
-                # The gaussian is not normalized, we want the center value to equal 1
-                g = np.exp(- ((x - x0) ** 2 + (y - y0) ** 2) / (2 * self.sigma ** 2))
-
-                # Usable gaussian range
-                g_x = max(0, -ul[0]), min(br[0], self.heatmap_size[0]) - ul[0]
-                g_y = max(0, -ul[1]), min(br[1], self.heatmap_size[1]) - ul[1]
-                # Image range
-                img_x = max(0, ul[0]), min(br[0], self.heatmap_size[0])
-                img_y = max(0, ul[1]), min(br[1], self.heatmap_size[1])
-
-                v = target_weight[joint_id]
-                if v > 0.5:
-                    target[joint_id][img_y[0]:img_y[1], img_x[0]:img_x[1]] = \
-                        g[g_y[0]:g_y[1], g_x[0]:g_x[1]]
+#                size = 2 * tmp_size + 1
+#                x = np.arange(0, size, 1, np.float32)
+#                y = x[:, np.newaxis]
+#                x0 = y0 = size // 2
+#                # The gaussian is not normalized, we want the center value to equal 1
+#                g = np.exp(- ((x - x0) ** 2 + (y - y0) ** 2) / (2 * self.sigma ** 2))
+#
+#                # Usable gaussian range
+#                g_x = max(0, -ul[0]), min(br[0], self.heatmap_size[0]) - ul[0]
+#                g_y = max(0, -ul[1]), min(br[1], self.heatmap_size[1]) - ul[1]
+#                # Image range
+#                img_x = max(0, ul[0]), min(br[0], self.heatmap_size[0])
+#                img_y = max(0, ul[1]), min(br[1], self.heatmap_size[1])
+#
+#                v = target_weight[joint_id]
+#                if v > 0.5:
+#                    target[joint_id][img_y[0]:img_y[1], img_x[0]:img_x[1]] = \
+#                        g[g_y[0]:g_y[1], g_x[0]:g_x[1]]
 
         return target, target_weight
